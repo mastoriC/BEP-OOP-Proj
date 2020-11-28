483a3066da275f3ba4477d495a806f30677b37dc
Artermis <nopphadon.phanwong@gmail.com>
2020-11-28 17:56:38 +0700

    add viewClient modules and create viewClient GUI

---
+++ Client-side/src/mainViewClient.java
@@ -0,0 +1,6 @@
+public class mainViewClient {
+
+    public static void main(String[] args) {
+        new viewClient();
+    }
+}
---
+++ Client-side/src/viewClient.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class viewClient {
+
+    //default constructor
+    public viewClient(){
+        JFrame fr;
+        JButton refresh, print, cancel;
+        JPanel queueList, optionPanel;
+        JLabel nameFile;
+
+        fr = new JFrame("BEP: Print for anywhere");
+
+
+
+
+
+        fr.pack();
+        fr.setVisible(true);
+
+    }
+}
