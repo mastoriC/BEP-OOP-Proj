5320eda3721ca97494f6a3266a1f9ee8b663919a
Artermis <nopphadon.phanwong@gmail.com>
2020-12-01 19:51:03 +0700

    test UI and try new layout

--- Client-side/src/viewClient.java
+++ Client-side/src/viewClient.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class viewClient {
 
@@ -6,16 +7,39 @@ public class viewClient {
     public viewClient(){
         JFrame fr;
         JButton refresh, print, cancel;
-        JPanel queueList, optionPanel;
-        JLabel nameFile;
+        JPanel queueListPanel, optionPanel;
+        JPanel southLayout, northLayout;
+        JLabel nameFile, list;
 
         fr = new JFrame("BEP: Print for anywhere");
+        fr.setLayout(new GridLayout(1,1));
+//        fr.setLayout(new BorderLayout());
+
+        queueListPanel = new JPanel();
+
+
+        list = new JLabel("test");
+        refresh = new JButton("refresh");
+
+        queueListPanel.setLayout(new BorderLayout());
+        queueListPanel.add(list, BorderLayout.NORTH);
+        queueListPanel.add(refresh, BorderLayout.SOUTH);
+
+
+        optionPanel = new JPanel();
+        nameFile = new JLabel("Files name to upload");
+        optionPanel.setLayout(new BorderLayout());
+        optionPanel.add(nameFile, BorderLayout.CENTER);
+
+
+        fr.add(queueListPanel);
+        fr.add(optionPanel);
+//        fr.add(queueListPanel, BorderLayout.WEST);
+//        fr.add(optionPanel, BorderLayout.EAST);
 
 
 
-
-
-        fr.pack();
+        fr.setSize(500,500);
         fr.setVisible(true);
 
     }
