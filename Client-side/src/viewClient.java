e3942e9b1ba6133d27d754d1ade128eb16a85821
Artermis <nopphadon.phanwong@gmail.com>
2020-12-03 14:08:05 +0700

    add Class Datagram and new layout

--- Client-side/src/viewClient.java
+++ Client-side/src/viewClient.java
@@ -12,32 +12,13 @@ public class viewClient {
         JLabel nameFile, list;
 
         fr = new JFrame("BEP: Print for anywhere");
-        fr.setLayout(new GridLayout(1,1));
-//        fr.setLayout(new BorderLayout());
+        fr.setLayout(new BorderLayout());
 
-        queueListPanel = new JPanel();
-
-
-        list = new JLabel("test");
         refresh = new JButton("refresh");
+        print = new JButton("print");
 
-        queueListPanel.setLayout(new BorderLayout());
-        queueListPanel.add(list, BorderLayout.NORTH);
-        queueListPanel.add(refresh, BorderLayout.SOUTH);
-
-
-        optionPanel = new JPanel();
-        nameFile = new JLabel("Files name to upload");
-        optionPanel.setLayout(new BorderLayout());
-        optionPanel.add(nameFile, BorderLayout.CENTER);
-
-
-        fr.add(queueListPanel);
-        fr.add(optionPanel);
-//        fr.add(queueListPanel, BorderLayout.WEST);
-//        fr.add(optionPanel, BorderLayout.EAST);
-
-
+        fr.add(refresh);
+        fr.add(print);
 
         fr.setSize(500,500);
         fr.setVisible(true);
