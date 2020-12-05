8d7a7fd425854c92e71fdcec001ac5a580c8c5a7
Artermis <nopphadon.phanwong@gmail.com>
2020-12-05 09:45:29 +0700

    update ViewClient

--- Client-side/src/viewClient.java
+++ Client-side/src/viewClient.java
@@ -6,9 +6,10 @@ public class viewClient {
     //default constructor
     public viewClient(){
         JFrame fr;
-        JButton refresh, print, cancel;
+        JButton refresh, print, cancel, choose;
         JPanel queueListPanel, optionPanel;
         JPanel southLayout, northLayout;
+        JTextArea queueList;
         JLabel nameFile, list;
 
         fr = new JFrame("BEP: Print for anywhere");
@@ -16,12 +17,38 @@ public class viewClient {
 
         refresh = new JButton("refresh");
         print = new JButton("print");
+        cancel = new JButton("Cancel");
 
-        fr.add(refresh);
-        fr.add(print);
+        queueList = new JTextArea();
+        queueList.setSize(500, 500);
+
+       queueListPanel = new JPanel();
+       optionPanel = new JPanel();
+
+       southLayout = new JPanel();
+       northLayout = new JPanel();
+
+       southLayout.add(print);
+       southLayout.add(cancel);
+
+       queueListPanel.setLayout(new BorderLayout());
+       queueListPanel.add(refresh, BorderLayout.SOUTH);
+       queueListPanel.add(queueList, BorderLayout.CENTER);
+
+       northLayout.setLayout(new FlowLayout());
+       nameFile = new JLabel("File to upload");
+       northLayout.add(nameFile);
+
+
+       fr.add(northLayout, BorderLayout.NORTH);
+       fr.add(queueListPanel, BorderLayout.WEST);
+       fr.add(optionPanel, BorderLayout.CENTER);
+       fr.add(southLayout, BorderLayout.SOUTH);
 
         fr.setSize(500,500);
         fr.setVisible(true);
+
+
 
     }
 }
